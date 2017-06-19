db13a33e80f0f7a7cf9ad0d85ef8ec40fe91fd35
Andrew Brown <andrewtcb@gmail.com>
2017-06-19 15:07:15 +1000

    second heroku commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,37 +17,3 @@ v1 = Venue.create name: "The Weeds", location: "300 Oxford St, Paddington", cate
 v2 = Venue.create name: "Horse and Cart", location: "12 Biggins St, Sydney", category: "Pub", description: "Lovely place for a pint", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmN5NbYXm_yAJRsoO-rSUjZisHDig7lFHBZO01n8FKiSmwcfYS"
 v3 = Venue.create name: "Special Bar", location: "66 Clemins St, Ryde", category: "Pub", description: "Nice beers", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgiBqNkq4R7prZfwvbjMXSpQWB3_3alLMlEcS1nVUgviOFE9moiw"
 v4 = Venue.create name: "Hand Bar", location: "3 Old St, Glebe", category: "Bar", description: "The scene to be seen", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY88hs478SRtW-20hxJd9yArNcgRxeho9YaFYG4O0Fug5UPmvU"
-
-
-Happyhour.destroy_all
-h1 = Happyhour.create start: Time.new(2017,6,19,17,10,0, "+09:00"), end: Time.new(2017,6,19,18,10,0, "+09:00")
-h2 = Happyhour.create start: Time.new(2017,6,20,17,10,0, "+09:00"), end: Time.new(2017,6,20,18,10,0, "+09:00")
-h3 = Happyhour.create start: Time.new(2017,6,21,17,10,0, "+09:00"), end: Time.new(2017,06,21,18,10,0, "+09:00")
-
-
-Deal.destroy_all
-d1 = Deal.create drink: "Beer", price: "3.50", description: "Becks, Carlton"
-d2 = Deal.create drink: "Wine", price: "5.50", description: "Red, White"
-d3 = Deal.create drink: "Spirits", price: "4.00", description: "Vodka, gin, brandy"
-
-
-u1.venues << v1 << v2
-u2.venues << v3
-u3.venues << v4
-
-
-
-v1.happyhours << h1 << h3
-v2.happyhours << h2 << h3
-v3.happyhours << h1 << h2 << h3
-v4.happyhours << h1
-
-
-
-h1.deals << d1
-h2.deals << d2
-
-
-d1.happyhours << h1
-d2.happyhours << h2
-d3.happyhours << h3
