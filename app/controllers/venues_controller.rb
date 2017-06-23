4be9a0cfe3c9ffa1c4b9da2fd534c35ca1969546
Andrew Brown <andrewtcb@gmail.com>
2017-06-23 10:37:44 +1000

    Maps added to venue pages

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -34,6 +34,13 @@ class VenuesController < ApplicationController
 
   def show
      @venue = Venue.find params["id"]
+     @hash = Gmaps4rails.build_markers(@venue) do |venue, marker|
+       location_link = view_context.link_to venue.name, venue_path(venue.id)
+       marker.lat venue.latitude
+       marker.lng venue.longitude
+       marker.json({:id => venue.id })
+       marker.infowindow "<p><strong><u>#{location_link}</u></strong></p><p></p><p>#{venue.location}</p>"
+     end
   end
 
   def index
